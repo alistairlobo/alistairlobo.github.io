d08e2e2cfcf02cd9d68ddfea56054132da21dd4a
Alistair Lobo <anonytr0janali9@gmail.com>
2018-11-05 16:19:22 +0530

    Optimised code files

---
+++ old-website-template/js (copy 1)/active.js
@@ -0,0 +1,16 @@
+// $(function() 
+// {
+// 	// $("h1").animate({
+//     //   "margin-left": "100px"
+//     // }, "slow");
+
+//     (".sidebar-menu li").on("click", function() {
+//        $(".sidebar-menu li").removeClass("active");
+//        $(this).addClass("active");
+//     });
+// });
+
+$(".sidebar-menu li").on("click", function() {
+    $(".sidebar-menu li").removeClass("active");
+    $(this).addClass("active");
+});
